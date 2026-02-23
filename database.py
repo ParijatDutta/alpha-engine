6b52721a1ebed0a4b749b37efd1b6bea328cac3b
ParijatDutta <83964332+ParijatDutta@users.noreply.github.com>
2026-02-23 05:14:31 +0000

    Fix 403 issue with user agent

--- database.py
+++ database.py
@@ -1,14 +1,21 @@
 import pandas as pd
 import os
+import requests
+from io import StringIO
 
 DATA_PATH = "sp500_metadata.parquet"
 
 def initialize_sp500():
-    """Pulls S&P 500 metadata from Wikipedia and saves locally."""
     if not os.path.exists(DATA_PATH):
-        # One-time pull of tickers and sectors
-        table = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
-        df = table[['Symbol', 'Security', 'GICS Sector', 'GICS Sub-Industry']]
+        url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+        # Pretend to be a browser to avoid 403 Forbidden
+        headers = {"User-Agent": "Mozilla/5.0"}
+        
+        response = requests.get(url, headers=headers)
+        # Use StringIO to avoid future pandas warnings
+        df = pd.read_html(StringIO(response.text))[0]
+        
+        df = df[['Symbol', 'Security', 'GICS Sector', 'GICS Sub-Industry']]
         df.to_parquet(DATA_PATH)
         return df
     return pd.read_parquet(DATA_PATH)
