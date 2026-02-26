4b7f54e3e797338342c92d90a965167c0e40256f
ParijatDutta <83964332+ParijatDutta@users.noreply.github.com>
2026-02-26 01:53:31 +0000

    Fixing error in variable name

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ with tab3:
         )
 
         # Add a column for Margin of Safety (MOS) percentage for the Treemap
-        df_alpha['MOS'] = (df_alpha['Intrinsic'] - df_alpha['Price']) / df_alpha['Price']
+        df_alpha['MOS'] = (df_alpha['intrinsic_value'] - df_alpha['Price']) / df_alpha['Price']
 
         st.subheader("🗺️ Sector Value Heatmap")
         st.caption("Size = Price Weight | Color = Alpha Score (Green = High Opportunity)")
